88d05d12804635bdebaaf7835fac5ee2fe428159
Lucas Frérot <lucas.frerot@imtek.uni-freiburg.de>
2023-07-18 14:48:02 +0200

    added comments in code

--- _notes/eigen/wave_mpi.cpp
+++ _notes/eigen/wave_mpi.cpp
@@ -1,19 +1,16 @@
 #include <Eigen/Core>
-#include <mpi.h>
 #include <array>
 #include <chrono>
 #include <iostream>
+#include <mpi.h>
 #include <sstream>
 
 using namespace Eigen;
 
+// RAII object for MPI init calls
 struct mpi_guard {
-  mpi_guard() {
-    MPI_Init(0, nullptr);
-  }
-  ~mpi_guard() {
-    MPI_Finalize();
-  }
+  mpi_guard() { MPI_Init(0, nullptr); }
+  ~mpi_guard() { MPI_Finalize(); }
 };
 
 int get_rank(MPI_Comm comm) {
@@ -28,24 +25,20 @@ int get_size(MPI_Comm comm) {
   return size;
 }
 
-bool is_left_rank(MPI_Comm comm) {
-  int rank = 0;
-  MPI_Comm_rank(comm, &rank);
-  return rank == 0;
-}
+bool is_left_rank(MPI_Comm comm) { return get_rank(comm) == 0; }
 
 bool is_right_rank(MPI_Comm comm) {
-  int rank = 0, size = 1;
-  MPI_Comm_rank(comm, &rank);
-  MPI_Comm_size(comm, &size);
-  return rank == size - 1;
+  return get_rank(comm) == get_size(comm) - 1;
 }
 
+// Scalar overload of central finite difference formula
 double second_order_fd(double left, double center, double right, double dx) {
   return (left - 2 * center + right) / (dx * dx);
 }
 
-ArrayXd second_order_fd(Ref<ArrayXd> left, Ref<ArrayXd> center, Ref<ArrayXd> right, double dx) {
+// Array overload of central finite difference formula
+ArrayXd second_order_fd(Ref<ArrayXd> left, Ref<ArrayXd> center,
+                        Ref<ArrayXd> right, double dx) {
   return (left - 2 * center + right) / (dx * dx);
 }
 
@@ -63,32 +56,45 @@ void apply_bc(MPI_Comm comm, Ref<ArrayXd> u_second, Ref<ArrayXd> u, double dx) {
 }
 
 /// Compute finite difference second derivative
-void second_derivative_fd(MPI_Comm comm, Ref<ArrayXd> u_second, Ref<ArrayXd> u, double dx) {
-  // Initiate communication
+void second_derivative_fd(MPI_Comm comm, Ref<ArrayXd> u_second, Ref<ArrayXd> u,
+                          double dx) {
+  // Communication setup:
+  // ghosts contains u values from left and right procs
+  // send contains u values to send to left and right procs
   Array2d ghosts, send;
-  ghosts = 0;
-  send(0) = u(0);
-  send(1) = u(u.size() - 1);
-  MPI_Neighbor_alltoall(send.data(), 1, MPI_DOUBLE,
-                        ghosts.data(), 1, MPI_DOUBLE, comm);
+  ghosts = 0;                 // initialize
+  send(0) = u(0);             // send to left
+  send(1) = u(u.size() - 1);  // send to right
+
+  // Communicate data with neighbors
+  MPI_Neighbor_alltoall(send.data(), 1, MPI_DOUBLE, ghosts.data(), 1,
+                        MPI_DOUBLE, comm);
+
+  // Nonblocking version of the communication (not necessarily faster)
+  // The idea is to initiate the comm before computing our own data
+  // so that when we are done the comm is finished
 
   // MPI_Request request;
   // MPI_Ineighbor_alltoall(send.data(), 1, MPI_DOUBLE,
   //                        ghosts.data(), 1, MPI_DOUBLE, comm, &request);
 
-  // Centered finite differences
+  // Centered finite differences on our own data
   auto n = u.size() - 2;
-  u_second.segment(1, n) = second_order_fd(u.head(n),
-                                           u.segment(1, n),
-                                           u.tail(n), dx);
+  u_second.segment(1, n) =
+      second_order_fd(u.head(n), u.segment(1, n), u.tail(n), dx);
+
+  // If nonblocking comms are used, we wait for ghost here
+  // MPI_Wait(&request, nullptr);
 
   // Compute second derivative involving ghosts
-  // MPI_Wait(&request, nullptr);
+  // For left and right ranks, some ghosts will be undefined
+  // That's ok, we initialized things to zero and values will be
+  // overwritten by the boundary conditions
   n = u.size() - 1;
-  u_second(0) = second_order_fd(ghosts(0), u(0), u(1), dx);
-  u_second(n) = second_order_fd(u(n - 1), u(n), ghosts(1), dx);
+  u_second(0) = second_order_fd(ghosts(0), u(0), u(1), dx);      // from left
+  u_second(n) = second_order_fd(u(n - 1), u(n), ghosts(1), dx);  // from right
 
-
+  // Finally apply boundary conditions
   apply_bc(comm, u_second, u, dx);
 }
 
@@ -106,30 +112,36 @@ void verlet_corr(Ref<ArrayXd> u_second, Ref<ArrayXd> u_first, Ref<ArrayXd> u,
 }
 
 /// Verlet full step
-void verlet(MPI_Comm comm, Ref<ArrayXd> u_second, Ref<ArrayXd> u_first, Ref<ArrayXd> u,
-            double dt, double dx) {
+void verlet(MPI_Comm comm, Ref<ArrayXd> u_second, Ref<ArrayXd> u_first,
+            Ref<ArrayXd> u, double dt, double dx) {
   verlet_pred(u_second, u_first, u, dt);
   second_derivative_fd(comm, u_second, u, dx);
   verlet_corr(u_second, u_first, u, dt);
 }
 
-void synchronized_out(MPI_Comm comm, Ref<ArrayXd> u) {
-  // Compute global size
-  int N = 0, local_N = u.size();
-  MPI_Reduce(&local_N, &N, 1, MPI_INT, MPI_SUM, 0, comm);
+// Gather all data and print array values
+// This is here for reference, but in a real world situation this
+// type of output routine should be avoided and parallel IO preferred
+void synchronized_out(MPI_Comm comm, int N, Ref<ArrayXd> u) {
+  int local_N = u.size();
 
+  // Gather local sizes and compute all offsets
   auto rank = get_rank(comm), size = get_size(comm);
   ArrayXi sizes(size), displ(size);
   MPI_Gather(&local_N, 1, MPI_INT, sizes.data(), 1, MPI_INT, 0, comm);
 
   for (int i = 1; i < size; ++i)
-      displ[i] = sizes[i] + displ[i - 1];
+    displ[i] = sizes[i] + displ[i - 1];
 
+  // Allocate receive buffer for rank 0
   ArrayXd u_gathered((rank == 0) ? N : 1);
-  MPI_Gatherv(u.data(), u.size(), MPI_DOUBLE,
-              u_gathered.data(), sizes.data(), displ.data(), MPI_DOUBLE,
-              0, comm);
 
+  // We need "v" variant of gather since not all processes have the same
+  // size of data (last one has more)
+  MPI_Gatherv(u.data(), u.size(), MPI_DOUBLE, u_gathered.data(), sizes.data(),
+              displ.data(), MPI_DOUBLE, 0, comm);
+
+  // Print only on root rank
   if (rank == 0)
     std::cout << u_gathered.transpose() << "\n";
 }
@@ -137,16 +149,17 @@ void synchronized_out(MPI_Comm comm, Ref<ArrayXd> u) {
 int main(int argc, char* argv[]) {
   mpi_guard guard{};  // Takes care of the calls to MPI_Init and MPI_Finalize
 
+  // Load arguments
   std::stringstream args;
   for (int i = 1; i < argc; ++i)
-      args << argv[i];
+    args << argv[i];
 
   // Problem definition
   int N = 100000;
 
   // Read from argument if present
   if (argc >= 2)
-      args >> N;
+    args >> N;
 
   auto L = 20.;
   auto dx = L / (N - 1);
@@ -156,7 +169,7 @@ int main(int argc, char* argv[]) {
   // Get some MPI context
   auto size = get_size(MPI_COMM_WORLD);
 
-  // Create a cartesian topology (a regular grid)
+  // Create a cartesian topology (a regular grid in 1d)
   MPI_Comm comm;
   std::array<int, 1> periodic{0};
   MPI_Cart_create(MPI_COMM_WORLD, 1, &size, periodic.data(), false, &comm);
@@ -165,7 +178,6 @@ int main(int argc, char* argv[]) {
   auto rank = get_rank(comm);
   auto local_N = N / size;
   auto local_L = L / size;
-  auto offset_N = rank * local_N;
   auto offset_L = rank * local_L;
 
   local_N += (rank == size - 1) ? N % size : 0;  // correct last rank
@@ -176,11 +188,11 @@ int main(int argc, char* argv[]) {
 
   if (is_right_rank(comm)) {
     x = ArrayXd::LinSpaced(local_N, start, end);
-  } else { // ignore endpoint
+  } else {  // ignore endpoint
     x = ArrayXd::LinSpaced(local_N + 1, start, end).head(local_N);
   }
 
-  // Work variables
+  // Work variables are local
   ArrayXd u(local_N), u_first(local_N), u_second(local_N);
 
   // Initial conditions
@@ -190,17 +202,20 @@ int main(int argc, char* argv[]) {
   second_derivative_fd(comm, u_second, u, dx);
 
   using namespace std::literals;
+  using clock = std::chrono::high_resolution_clock;
 
-  const auto tik = std::chrono::high_resolution_clock::now();
-
+  // We time our run loop
+  const auto tik = clock::now();
   for (auto i = 0; i < steps; ++i) {
     // synchronized_out(comm, u);
     verlet(comm, u_second, u_first, u, dt, dx);
   }
+  const auto tok = clock::now();
 
-  const auto tok = std::chrono::high_resolution_clock::now();
-
+  // Print loop time on root rank (in miliseconds)
   if (rank == 0) {
     std::cout << (tok - tik) / 1ms;
   }
+
+  return 0;
 }
